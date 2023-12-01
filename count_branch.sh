adb2211f7ed2635c1e1a5b850d76a0c4ff0fbcbe
vincentchan <hkvincent@users.noreply.github.com>
2023-12-01 18:23:38 +0800

    Create count_branch.sh
    add count branch

---
+++ count_branch.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Initialize an array for the exclusion patterns
+declare -a exclude_patterns=()
+
+# Process each parameter until "--" is found, considering these as file types to exclude
+while [ -n "$1" ] && [ "$1" != "--" ]; do
+  exclude_patterns+=("*.$1")
+  shift
+done
+
+# Skip the "--" separator
+[ "$1" == "--" ] && shift
+
+# The next parameter is the branch name
+branch_name=$1
+shift
+
+# Optional merge base, defaulting to master if not specified
+merge_base=${1:-master}
+
+# Fetch all commit hashes from the specified range
+commit_hashes=$(git rev-list $(git merge-base $merge_base $branch_name)..$branch_name)
+
+# Declare an associative array to store results
+declare -A results
+
+# Loop over each commit hash
+for commit_hash in $commit_hashes; do
+  # Get a list of all changes in the commit with stats
+  all_changes=$(git show --numstat --pretty="" $commit_hash)
+
+  # Read through each line of all_changes
+  while read -r add del file; do
+    # Skip if add or del are not numbers
+    if ! [[ $add =~ ^[0-9]+$ ]] || ! [[ $del =~ ^[0-9]+$ ]]; then
+      continue
+    fi
+
+    # Check if file matches any of the exclusion patterns
+    for pattern in "${exclude_patterns[@]}"; do
+      if [[ $file == $pattern ]]; then
+        # Skip this file if it matches the exclusion pattern
+        continue 2
+      fi
+    done
+
+    # Get the author of the change for the current file in the commit
+    author=$(git log -1 --format="%an" $commit_hash -- "$file")
+
+    # Accumulate the stats in the results array for the author
+    ((results["$author,add"]+=add))
+    ((results["$author,del"]+=del))
+  done <<< "$all_changes"
+done
+
+# Print the results for each author
+for key in "${!results[@]}"; do
+  IFS=',' read -ra key_parts <<< "$key"
+  author=${key_parts[0]}
+  type=${key_parts[1]}
+
+  if [ "$type" = "add" ]; then
+    total=$((results["$author,add"] - results["$author,del"]))
+    echo "${total} Total lines, Added lines: ${results[$author,add]}, Removed lines: ${results[$author,del]}, Member: $author"
+  fi
+done | sort -nr
