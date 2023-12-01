73c6c4650338e5094ef3fb9e18fdd01f7a847c5f
vincentchan <hkvincent@users.noreply.github.com>
2023-12-01 17:40:24 +0800

    Update count_commit.sh

--- count_commit.sh
+++ count_commit.sh
@@ -1,42 +1,46 @@
 #!/bin/bash
 
-# Assign the commit hash and optional file types to exclude from the parameters
-commit_hash=$1
-shift
-
 # Initialize an array for the exclusion patterns
 declare -a exclude_patterns=()
 
-# Process each file type parameter to exclude
-while [ -n "$1" ]; do
-  # Add the exclusion pattern for the current parameter to the array
+# Process each parameter until "--" is found, considering these as file types to exclude
+while [ -n "$1" ] && [ "$1" != "--" ]; do
   exclude_patterns+=("*.$1")
   shift
 done
 
-# Get a list of all changes in the commit with stats
-all_changes=$(git show --numstat --pretty="" $commit_hash)
+# Skip the "--" separator
+[ "$1" == "--" ] && shift
+
+# The remaining parameters are treated as commit hashes
+commit_hashes=("$@")
 
 # Declare an associative array to store results
 declare -A results
 
-# Read through each line of all_changes
-while read -r add del file; do
-  # Check if file matches any of the exclusion patterns
-  for pattern in "${exclude_patterns[@]}"; do
-    if [[ $file == $pattern ]]; then
-      # Skip this file if it matches the exclusion pattern
-      continue 2
-    fi
-  done
+# Loop over each commit hash
+for commit_hash in "${commit_hashes[@]}"; do
+  # Get a list of all changes in the commit with stats
+  all_changes=$(git show --numstat --pretty="" $commit_hash)
 
-  # Get the author of the change for the current file in the commit
-  author=$(git log -1 --format="%an" $commit_hash -- "$file")
+  # Read through each line of all_changes
+  while read -r add del file; do
+    # Check if file matches any of the exclusion patterns
+    for pattern in "${exclude_patterns[@]}"; do
+      if [[ $file == $pattern ]]; then
+        # Skip this file if it matches the exclusion pattern
+        continue 2
+      fi
+    done
 
-  # Accumulate the stats in the results array for the author
-  ((results["$author,add"]+=add))
-  ((results["$author,del"]+=del))
-done <<< "$all_changes"
+    # Get the author of the change for the current file in the commit
+    author=$(git log -1 --format="%an" $commit_hash -- "$file")
+
+    # Accumulate the stats in the results array for the author
+    ((results["$author,add"]+=add))
+    ((results["$author,del"]+=del))
+  done <<< "$all_changes"
+done
 
 # Print the results for each author
 for key in "${!results[@]}"; do
@@ -48,4 +52,4 @@ for key in "${!results[@]}"; do
     total=$((results["$author,add"] - results["$author,del"]))
     echo "${total} Total lines, Added lines: ${results[$author,add]}, Removed lines: ${results[$author,del]}, Member: $author"
   fi
-done | sort -n -r
+done | sort -nr
